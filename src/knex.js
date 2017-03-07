8c4601741192b39ac174282d762ea035d0f23f61
Nathan Miller <nathan.miller@manageflitter.com>
2017-03-07 17:01:45 +1100

    need to pick db based on env

--- src/knex.js
+++ src/knex.js
@@ -1,5 +1,5 @@
 import knex from 'knex';
-import {development} from '../knexfile';
+import {development, production} from '../knexfile';
 
-const db = knex(development);
+const db = knex(process.env.NODE_ENV === 'production' ? production : development);
 export default db;
